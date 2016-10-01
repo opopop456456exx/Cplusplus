4e6779d42fca392a8fb94d69fc87a2578a6129d5
opopop456456exx <573967889@qq.com>
2016-10-01 10:43:08 +0800

    2

--- Cpulspuls/Cpulspuls/源.cpp
+++ Cpulspuls/Cpulspuls/源.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+using namespace std;
+
+int valplus(int a,int b);
+
 
 int main(void)
 {
-	std::cout << "HELLO WORLD!"<< std::endl;
-	std::cin.get();
+	int sum;
+	int number1=10;
+	int number2 = 20;
+	cout << number1 << " " << number2 << endl;
+	sum=valplus(number1, number2);
+	cout << number1 << " " << number2 << endl;
+	cout << sum<<endl;
+	
+//	cin >> number1;
+
 	return 0;
+	
+}
+
+
+int valplus(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	
+	return a+b;
 }
